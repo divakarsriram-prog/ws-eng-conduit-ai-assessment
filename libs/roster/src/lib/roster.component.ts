4e1d68ddbbae6fa3cd50db512af6015fbdabc339
divakarsriram-prog <sriramd7392@yahoo.com>
2026-02-28 17:22:59 +0000

    chore(conduit): Generates patch.

--- libs/roster/src/lib/roster.component.ts
+++ libs/roster/src/lib/roster.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+type RosterEntry = {
+  username: string;
+  profileLink: string;
+  articlesAuthoredCount: number;
+  favoritesReceivedCount: number;
+  firstArticleDate: string | null;
+};
 
 @Component({
   selector: 'realworld-roster',
   templateUrl: './roster.component.html',
-  styleUrls: [],
-  providers: [],
-  imports: [],
   standalone: true,
+  imports: [CommonModule, RouterModule],
 })
-export class RosterComponent {}
+export class RosterComponent implements OnInit {
+  roster: RosterEntry[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+
+    this.http.get<RosterEntry[]>('/api/users/roster').subscribe({
+      next: (data) => {
+        this.roster = data ?? [];
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load roster.';
+        this.loading = false;
+      },
+    });
+  }
+}
